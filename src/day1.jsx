b67a2ad430c7a371467f7f7fba27503199908339
joanhui <joanhui.chiu@gmail.com>
2023-08-12 17:39:05 +0800

    git init

---
+++ src/day1.jsx
@@ -0,0 +1,22 @@
+// 練習區塊ㄧ
+function formatName(user) {
+    return user.firstName + " " + user.lastName;
+  }
+  // 區塊結束
+  
+  const user = {
+    firstName: "Candy",
+    lastName: "Wang",
+    description: "前端工程師"
+  };
+  
+  // 練習區塊二
+  const element = (
+    <h1>
+      {formatName(user)} 是一位 {user.description}
+    </h1>
+  );
+  // 區塊結束
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(element);
